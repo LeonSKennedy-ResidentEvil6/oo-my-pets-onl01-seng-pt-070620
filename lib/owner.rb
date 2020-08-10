044f62e7ab71234cb0f20f37e4a1fbf13e490218
Ebo Lee <yiboli.coding@gmail.com>
2020-08-10 01:12:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -78,7 +78,7 @@ class Owner
 
   def list_pets
     
-    "I have #{@owner.dogs.count} dog(s), and #{@owner.cats.count} cat(s)."
+    "I have #{dogs.count} dog(s), and #{@owner.cats.count} cat(s)."
 
   end
 
